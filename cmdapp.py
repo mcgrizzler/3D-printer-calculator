07266d8e330bb7404493170aee79ff009d2cdea4
Matt G <mattgravis@gmail.com>
2019-02-20 00:55:18 -0800

    Fixed logic and added PID tune script

--- cmdapp.py
+++ cmdapp.py
@@ -1,8 +1,8 @@
 #########################################################
 #              -- Rep Rap Calibrator --                 #
 #               Author: Matthew Gravis                  #
-#                   Date: 2/18/19                       #
-#                      Ver: 0.1                         #
+#                   Date: 2/20/19                       #
+#                      Ver: 0.2                         #
 #########################################################
 
 # -*- coding: utf-8 -*-
@@ -57,26 +57,36 @@ actions = [
                 'name': 'EEPROM Values' # Display modified EEPROM values for Maker Select V2.1
             },
         ],
-        'validate': lambda answer: 'You must choose at least one action.' \
-            if len(answer) == 0 else True
     }
 ]
 
 # - Functions - 
-def main(): # Main program function
+def main(firstRun): # Main program function
     # Check if to display intro
     if firstRun:
         intro()
-        firstRun = False
     else:
         print('\n\n')
 
     # Run actionSelection prompt to select function
     actionSelection = prompt(actions, style=style)
 
-    # --Debug-- Print
-    pprint(actionSelection)
-
+    # Check for input
+    if actionSelection['script'] == '':
+        print('You must choose an action.\n')
+        time.sleep(2)
+        main(False)
+    else:
+        if actionSelection['script'][0] == 'Calibrate All':
+            calibrateAll()
+        elif actionSelection['script'][0] == 'PID Tune':
+            pidTune(True)
+        elif actionSelection['script'][0] == 'Extruder':
+            calibrateExtruder(True)
+        elif actionSelection['script'][0] == 'Linear Advance':
+            calibrateLinearAdvance()
+        elif actionSelection['script'][0] == 'EEPROM Values':
+            displayEEPROM()
 
 def intro(): # Program intro display
     print('\n----------\n\nRepRap Calibrator\n\n----------')
@@ -90,13 +100,42 @@ def calibrateAll(): # Run all calibrations
 
 def pidTune(singleRun): # PID tune extruder and bed
     print('\n\n\n\n\n\n\n\nPID Tuning Starting...\n\n')
+
+    print('Tools Needed:\n\n-Ruler or Calipers\n-Marker')
+    print('Preparation:\n\n-Heat Extruder (PLA: 210C / ABS: 240C\n-Load a light colored filament (Or dark if you have a light marker ;))\n\n')
     
-    # Placeholder :)
+    temp_continue = input('Press enter to continue...')
+
+    print('\nStep 1:\n\n-Measure 20mm from the top of the extruder and mark the filament.\n-Measure 100mm from the first mark and mark the filament.\n\n')
+    
+    temp_continue = input('Press enter to continue...')
+
+    print('\nStep 2:\n\n-Select Move E-Axis on printer control box (1 mm)\n-IMPORTANT: Go Slowly.\n-Move the extruder 100mm one mm at a time\n\n')
+
+    temp_continue = input('Press enter to continue...')
+
+    print('\nStep 3:\n\n-Measure from the top of the extruder to the remaining mark.\n\n')
+    
+    actualMovement = input('Enter the measurement in mm: ')
+
+    print('\nStep 4:\n\n-Launch octoprint or pronterface and start a usb session with the printer\n-Open the Terminal\n-Send Command M503 to the printer\n-Find the line starting with M92\n\n')
+
+    setMovement = input('Enter the current value of E on line M92: ')
+
+    newE = 95
+
+    print('\nStep 5:\n\n-Send the printer the following command: M92 E{}\n-Send M503 again and ensure the value has changed for M92 E\n-Send command M500 to save the values\n\n'.format(newE))
+
+    temp_continue = input('Press enter to continue...')
+
+    print('\nPID Tuning Complete!\n\n')
+    
+    time.sleep(2)
 
     if singleRun:
-        main():
+        main(False)
     else:
-        calibrateExtruder():
+        calibrateExtruder(False)
 
 def calibrateExtruder(singleRun): # Calibrate Extruder E-Steps
     print('\n\n\n\n\n\n\n\nExtruder Calibration Starting...\n\n')
@@ -104,24 +143,24 @@ def calibrateExtruder(singleRun): # Calibrate Extruder E-Steps
     # Placeholder :)
 
     if singleRun:
-        main():
+        main(False)
     else:
-        calibrateLinearAdvance():
+        calibrateLinearAdvance()
 
 def calibrateLinearAdvance(): # Calibrate K-Value and enable Linear Advance
     print('\n\n\n\n\n\n\n\nLinear Advance Calibration Starting...\n\n')
 
     # Placeholder :)
 
-    main():
+    main(False)
 
 def displayEEPROM(): # Show EEPORM Values
     print('\n\n\n\n\n\n\n\nEEPROM Values Starting...\n\n')
 
     # Placeholder :)
 
-    main():
+    main(False)
 
 # - Main Program Call -
-main()
+main(True)
     
