2407b1eca9d2c71b7b73d26eb74d089a5c48d6fb
Matt G <mattgravis@gmail.com>
2019-02-20 02:22:26 -0800

    Partial code for PID Tune

--- cmdapp.py
+++ cmdapp.py
@@ -15,11 +15,6 @@ from pprint import pprint
 from PyInquirer import style_from_dict, Token, prompt, Separator
 from PyInquirer import Validator, ValidationError
 
-# - Variables -
-
-# Booleans
-firstRun = True # True = Show Intro || False == Two Return-lines
-
 # - Styles -
 style = style_from_dict({
     Token.Separator: '#f0ff00',
@@ -99,48 +94,94 @@ def calibrateAll(): # Run all calibrations
     pidTune(False) # Run PID tuning without Single Run
 
 def pidTune(singleRun): # PID tune extruder and bed
-    print('\n\n\n\n\n\n\n\nPID Tuning Starting...\n\n')
+    print('\n\n\n\n\n\n\n\nPID Tuning Starting...\n\n----------\n')
 
-    print('Tools Needed:\n\n-Ruler or Calipers\n-Marker')
-    print('Preparation:\n\n-Heat Extruder (PLA: 210C / ABS: 240C\n-Load a light colored filament (Or dark if you have a light marker ;))\n\n')
-    
-    temp_continue = input('Press enter to continue...')
+    time.sleep(1)
 
-    print('\nStep 1:\n\n-Measure 20mm from the top of the extruder and mark the filament.\n-Measure 100mm from the first mark and mark the filament.\n\n')
-    
-    temp_continue = input('Press enter to continue...')
-
-    print('\nStep 2:\n\n-Select Move E-Axis on printer control box (1 mm)\n-IMPORTANT: Go Slowly.\n-Move the extruder 100mm one mm at a time\n\n')
-
-    temp_continue = input('Press enter to continue...')
-
-    print('\nStep 3:\n\n-Measure from the top of the extruder to the remaining mark.\n\n')
-    
-    actualMovement = input('Enter the measurement in mm: ')
-
-    print('\nStep 4:\n\n-Launch octoprint or pronterface and start a usb session with the printer\n-Open the Terminal\n-Send Command M503 to the printer\n-Find the line starting with M92\n\n')
-
-    setMovement = input('Enter the current value of E on line M92: ')
-
-    newE = 95
-
-    print('\nStep 5:\n\n-Send the printer the following command: M92 E{}\n-Send M503 again and ensure the value has changed for M92 E\n-Send command M500 to save the values\n\n'.format(newE))
-
-    temp_continue = input('Press enter to continue...')
-
-    print('\nPID Tuning Complete!\n\n')
+    print('Step 1:\n\n-Launch octoprint or pronterface and start a usb session with the printer\n-Send the following command to the printer: (PLA) M303 E0 S210 C5 (ABS) M303 E0 S245 C5\n\n')
     
     time.sleep(2)
+    temp_continue = input('Press enter to continue...')
+    time.sleep(1)
 
+    eP = input('Enter Kp Value: ')
+    eI = input('Enter Ki Value: ')
+    eD = input('Enter Kd Value: ')
+
+    time.sleep(1)
+
+    print('\nEnter the following command:\n\n- M301 P{} I{} D{}\n\n- M500\n\n'.format(eP, eI, eD))
+
+    time.sleep(2)
+    temp_continue = input('Press enter to continue...')
+    time.sleep(1)
+
+    print('\n-Send the following command to the printer: (PLA) M303 E-1 S60 C5 (ABS) M303 E-1 S70 C5\n\n')
+   
+   
+    # !! STOP POINT !! -- You were continuing the script to PID tune a second time with higher values and lower height/high fan --
+   
+   
+    time.sleep(1)
+
+    bP = input('Enter Kp Value: ')
+    bI = input('Enter Ki Value: ')
+    bD = input('Enter Kd Value: ')
+
+    print('\nEnter the following command:\n\n- M304 P{} I{} D{}\n\n- M500\n\n'.format(bP, bI, bD))
+    
     if singleRun:
         main(False)
     else:
         calibrateExtruder(False)
 
 def calibrateExtruder(singleRun): # Calibrate Extruder E-Steps
-    print('\n\n\n\n\n\n\n\nExtruder Calibration Starting...\n\n')
+    print('\n\n\n\n\n\n\n\nExtruder Calibration Starting...\n\n----------\n')
     
-    # Placeholder :)
+    time.sleep(1)
+
+    print('Tools Needed:\n\n-Ruler or Calipers\n-Marker\n')
+    print('Preparation:\n\n-Heat Extruder (PLA: 210C / ABS: 240C)\n-Load a light colored filament (Or dark if you have a light marker ;))\n\n')
+    
+    time.sleep(2)
+    temp_continue = input('Press enter to continue...')
+    time.sleep(1)
+
+    print('\n\nStep 1:\n\n-Measure 20mm from the top of the extruder and mark the filament.\n-Measure 100mm from the first mark and mark the filament.\n\n')
+    
+    time.sleep(2)
+    temp_continue = input('Press enter to continue...')
+    time.sleep(1)
+
+    print('\n\nStep 2:\n\n-Select Move E-Axis on printer control box (1 mm)\n-IMPORTANT: Go Slowly.\n-Move the extruder 100mm one mm at a time\n\n')
+
+    time.sleep(2)
+    temp_continue = input('Press enter to continue...')
+    time.sleep(1)
+
+    print('\n\nStep 3:\n\n-Measure from the top of the extruder to the remaining mark.\n\n')
+    
+    time.sleep(2)
+    actualMovement = input('Enter the measurement in mm: ')
+    time.sleep(1)
+
+    print('\n\nStep 4:\n\n-Launch octoprint or pronterface and start a usb session with the printer\n-Open the Terminal\n-Send Command M503 to the printer\n-Find the line starting with M92\n\n')
+
+    time.sleep(2)
+    setMovement = input('Enter the current value of E on line M92: ')
+    time.sleep(1)
+
+    newE = 95
+
+    print('\nStep 5:\n\n-Send the printer the following command: M92 E{}\n-Send M503 again and ensure the value has changed for M92 E\n-Send command M500 to save the values\n\n'.format(newE))
+    
+    time.sleep(2)
+    temp_continue = input('Press enter to continue...')
+    time.sleep(1)
+
+    print('\n\nExtruder Calibration Complete!')
+    
+    time.sleep(2)
 
     if singleRun:
         main(False)
